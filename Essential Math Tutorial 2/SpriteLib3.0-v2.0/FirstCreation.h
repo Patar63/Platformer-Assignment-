9a576f4b43d7430f009815427f55d56660da70cf
Patrick Thompson <patrick.thompson8@ontariotechu.net>
2020-10-14 17:13:05 -0400

    This is the base code given to us. Unaltered.

---
+++ Essential Math Tutorial 2/SpriteLib3.0-v2.0/FirstCreation.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Scene.h"
+
+class FirstCreation : public Scene
+{
+public:
+	FirstCreation(std::string name);
+
+	void Unload();
+
+	void InitScene(float windowWidth, float windowHeight) override;
+
+	void Update() override;
+
+	void AdjustScrollOffset();
+
+	//Input overrides
+	void KeyboardHold() override;
+	void KeyboardDown() override;
+	void KeyboardUp() override;
+
+private:
+	int mainPlayer = 0;
+};
---
+++ Essential Math Tutorial 2/SpriteLib3.0-v2.0/PhysicsPlayground.cpp
@@ -0,0 +1,29 @@
+#include "PhysicsPlayground.h"
+
+PhysicsPlayground::PhysicsPlayground(std::string name)
+{
+}
+
+void PhysicsPlayground::InitScene(float windowWidth, float windowHeight)
+{
+}
+
+void PhysicsPlayground::Update()
+{
+}
+
+void PhysicsPlayground::AdjustScrollOffset()
+{
+}
+
+void PhysicsPlayground::KeyboardHold()
+{
+}
+
+void PhysicsPlayground::KeyboardDown()
+{
+}
+
+void PhysicsPlayground::KeyboardUp()
+{
+}
